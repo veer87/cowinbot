06d198c1513d6b055fef2c95ae06af540e6610be
Avdhesh Rana <avrana@paypal.com>
2021-05-04 17:43:33 +0530

    logging code

--- src/main/java/org/covid19/india/CowinBot/CowinBotApplication.java
+++ src/main/java/org/covid19/india/CowinBot/CowinBotApplication.java
@@ -1,8 +1,10 @@
 package org.covid19.india.CowinBot;
 
 import org.covid19.india.CowinBot.service.Bot;
+import org.covid19.india.CowinBot.service.CowinApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
@@ -12,17 +14,19 @@ public class CowinBotApplication {
 
 	public static void main(String[] args) {
 
-		ApiContextInitializer.init();
+		/*ApiContextInitializer.init();
 		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
 		try {
 			telegramBotsApi.registerBot(new Bot());
 		} catch (TelegramApiRequestException e) {
 			e.printStackTrace();
-		}
+		}*/
 
 
-	//	Consumer.getAppointmentAvailabilityByPincode("474001", "01-05-2021");
-
+		ResponseEntity<String> responseEntity1 = CowinApi.getCalendarByPin("474001", "01-05-2021");
+		ResponseEntity<String> responseEntity2 = CowinApi.getFindByPin("474001", "01-05-2021");
+		System.out.println("----------ResponseE1---"+responseEntity1.getBody()+"\n"+
+							"-------Respe2---- " + responseEntity2.getBody());
 		SpringApplication.run(CowinBotApplication.class, args);
 	}
 
